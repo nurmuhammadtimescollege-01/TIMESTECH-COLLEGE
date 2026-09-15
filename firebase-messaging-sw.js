2da4e205cbd714e51dfcdeefe7b69c7024707992
nurmuhammadtimescollege-01 <nurmuhammad.timescollege@gmail.com>
2026-09-15 15:49:34 +0800

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,18 @@
+importScripts(
+    "https://www.gstatic.com/firebasejs/12.19.0/firebase-app-compat.js"
+);
+
+importScripts(
+    "https://www.gstatic.com/firebasejs/12.19.0/firebase-messaging-compat.js"
+);
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDbmmHXlulqgUxqzbn7iGeywHMdEraDO2Y",
+    authDomain: "student-area-2dd83.firebaseapp.com",
+    projectId: "student-area-2dd83",
+    storageBucket: "student-area-2dd83.firebasestorage.app",
+    messagingSenderId: "601486438219",
+    appId: "1:601486438219:web:ac9e234cc153b9cd59c2ce"
+});
+
+const messaging = firebase.messaging();
